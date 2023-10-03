3cb30545a9732f21afacbf5fd89be9b7f60ef16a
CelalU <cll-ugr@hotmail.com>
2023-10-03 23:59:17 +0300

    update

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ arraws.forEach((arrow, i) => {
     movieLists[i].computedStyleMap.trasform = "translateX(-300px)";
   });
 });
+
+function addTranslateX() {
+  var myDiv = document.getElementById("myDiv");
+  if (myDiv && !myDiv.style.transform) {
+    myDiv.style.transform = "translateX(-500px)";
+  } else {
+    myDiv.style.transform = "translateX(0px)";
+  }
+}
+function addTranslateX2() {
+  var myDiv = document.getElementById("myDiv2");
+  if (myDiv && !myDiv.style.transform) {
+    myDiv.style.transform = "translateX(-500px)";
+  } else {
+    myDiv.style.transform = "translateX(0px)";
+  }
+}
+function addTranslateX3() {
+  var myDiv = document.getElementById("myDiv3");
+  if (myDiv && !myDiv.style.transform) {
+    myDiv.style.transform = "translateX(-500px)";
+  } else {
+    myDiv.style.transform = "translateX(0px)";
+  }
+}
